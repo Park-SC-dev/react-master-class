06c3762dee9f0a662e2b872b165c98d3dbde0b72
Park-SC-dev <ajt585@gmail.com>
2025-04-01 01:09:17 +0900

    애니메이션, 셀렉터 공부

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props)=> props.bgcolor};
-  width: 100px;
-  height: 100px;
+const animation = keyframes`
+  0%{
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius: 0px;    
+  }
 `;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`
-
-
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color:tomato;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  animation:${animation} 1s linear infinite;
+  span {
+    font-size: 50px;
+    &:hover {
+      font-size: 100px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
 
 function App() {
   return (
-    <Father>
-      <Box bgcolor="teal"/>
-      <Circle bgcolor="tomato"/>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🐷</span>
+      </Box>
+    </Wrapper>
   );
 }
 
